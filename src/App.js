ce18a2d14462899f57788b9507e43126e50bae84
fsansalvadore <f.sansalvadore@gmail.com>
2020-06-01 18:51:29 +0200

    added shop page and started header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header/>
+      <Switch>
+        <Route path="/shop">
+          <ShopPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
